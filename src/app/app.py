ae1a85fecd21eb48520a1842668896fb0eeec9e5
ericding <ericding0110@gmail.com>
2021-11-09 16:07:16 +0800

    Adjustment

--- src/app/app.py
+++ src/app/app.py
@@ -1,5 +1,4 @@
 from flask import Flask
-# from src.app.config import Config
 from flask_sqlalchemy import SQLAlchemy
 
 application = Flask(
@@ -7,8 +6,8 @@ application = Flask(
     template_folder='../templates',
     static_folder='../static',
 )
+# set up the database location
 application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mapData.db'
 application.config['SECRET_KEY'] = '126059605bb3b26263f9a46e'
+# create an database object based on current application configuration
 db = SQLAlchemy(application)
-# application.config.from_object(Config)
-# db = SQLAlchemy(application)
